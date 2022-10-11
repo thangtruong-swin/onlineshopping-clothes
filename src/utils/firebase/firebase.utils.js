caee524402f73175517d9e767d6eb9461f9fc884
Thang Truong <thang.truong.swin@gmail.com>
2022-10-11 22:12:17 +1100

    user registered

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -6,6 +6,8 @@ import {
 	signInWithPopup,
 } from "firebase/auth";
 
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+
 const firebaseConfig = {
 	apiKey: "AIzaSyAm9OYXvPhW5vq5I79yd5cLcBu2fsY91sQ",
 	authDomain: "onlineshopping-clothes.firebaseapp.com",
@@ -26,3 +28,26 @@ provider.setCustomParameters({
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+	const userDocRef = doc(db, "user", userAuth.uid);
+	// console.log(userDocRef);
+
+	const userSnapshot = await getDoc(userDocRef);
+	// console.log(userSnapshot);
+	// console.log(userSnapshot.exists());
+
+	if (!userSnapshot.exists()) {
+		const { displayName, email } = userAuth;
+		const createAt = new Date();
+		try {
+			await setDoc(userDocRef, { displayName, email, createAt });
+		} catch (error) {
+			console.log("Error creating the user", error.message);
+		}
+	}
+
+	return userDocRef;
+};
